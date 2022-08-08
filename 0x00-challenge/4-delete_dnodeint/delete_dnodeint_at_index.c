6bcfe8837010a87a749659e4bd0a87b8cfc0e19e
Adeniyii <ifedayoadeniyi@gmail.com>
2022-08-08 16:47:23 +0100

    fix: implement correct deletion

--- 0x00-challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
+++ 0x00-challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
@@ -43,10 +43,12 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	}
 	else
 	{
-		(*head)->prev->prev = (*head)->prev;
-		free(*head);
 		if ((*head)->next)
+		{
+			(*head)->prev->next = (*head)->next;
 			(*head)->next->prev = (*head)->prev;
+		}
+		free(*head);
 		*head = saved_head;
 	}
 	return (1);
